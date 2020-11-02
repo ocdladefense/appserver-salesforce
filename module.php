78307cf1e4dba813c0cff763dcec0f83c5606929
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2020-11-02 11:40:29 -0800

    module.php cleanup

--- module.php
+++ module.php
@@ -17,18 +17,6 @@ class SalesforceModule extends Module{
 function salesforceModRoutes()
 {   
     $salesforceModRoutes = array(
-        "authorize" => array(
-            "callback" => "authorize",
-            "files" => array()
-        ),
-        "authorize-user" => array(
-            "callback" => "authorize_user",
-            "files" => array()
-        ),
-        "oauth-callback" => array(
-            "callback" => "oauth_callback",
-            "files" => array()
-        ),
         "generate-order" => array(
             "callback" => "generateOrder",
             "files" => array()
@@ -36,162 +24,13 @@ function salesforceModRoutes()
         "customer-profile-id" => array(
             "callback" => "getCustomerProfileIdFromSalesforce",
             "files" => array()
-        ),
-        "get-object-by-id" => array(
-            "callback" => "getObjectById",
-            "files" => array()
         )
     );
     return $salesforceModRoutes;
 }
 
-function authorize()
-{
-    $auth_url = LOGIN_URI
-        . "/services/oauth2/authorize?response_type=code&client_id="
-        . CLIENT_ID . "&redirect_uri=" . urlencode(REDIRECT_URI);   
-        header('Location: ' . $auth_url);
-}
 
-function authorize_user()
-{
-    $response = getOauthTokenWithPassword();
-    $json = $response->getPhpArray();
-    $_SESSION['access_token'] = $json['access_token'];
-    $_SESSION['instance_url'] = $json['instance_url'];
-}
 
-function oauth_callback()
-{
-    $response = getOauthTokenWithCallback();
-    $json = $response->getPhpArray();
-    $_SESSION['access_token'] = $json['access_token'];
-    $_SESSION['instance_url'] = $json['instance_url'];
-
-}
-
-function getOauthTokenWithCallback()
-{
-    $code = $_GET['code'];
-
-    if (!isset($code) || $code == "") 
-    {
-        die("Error - code parameter missing from request!");
-    }
-
-    $params =  array
-    (
-        'code' => $code,
-        'grant_type' => 'authorization_code',
-        'client_id' => CLIENT_ID,
-        'client_secret' => CLIENT_SECRET,
-        'redirect_uri' => urlencode(REDIRECT_URI)
-    );
-    return getOauthToken($params);
-}
-
-function getOauthTokenWithPassword()
-{
-    $params =  array
-    (
-        //'code' => $code,
-        'grant_type' => 'password',
-        'client_id' => CLIENT_ID,
-        'client_secret' => CLIENT_SECRET,
-        'username' => SALESFORCE_USERNAME,
-        'password' => SALESFORCE_PASSWORD,
-        'redirect_uri' => urlencode(REDIRECT_URI)
-    );
-    return getOauthToken($params);
-}
-
-function getOauthToken($params)
-{
-    $username = $params["username"];
-
-    $token_url = LOGIN_URI . "/services/oauth2/token";
-    $request = new HTTPRequest($token_url);
-    $request->setParams($params);
-
-    $request->setPost();
-    $response = $request->makeHTTPRequest();
-    $phpResponse = $response->getPhpArray();
-
-    if(!empty($phpResponse['error'])) throw new exception($username."   ".$phpResponse['error_description']);
-    return $response;
-}
-//Get a salesforce database sObject by Id
-function getObjectById($objectType, $objectId) 
-{
-    $response = getOauthTokenWithPassword();
-    $instance_url = $response->getPhpArray()['instance_url'];
-    $access_token = $response->getPhpArray()['access_token'];
-
-    try{
-        $response = new stdClass();
-        $response->object = null;
-    
-        $url = "$instance_url/services/data/v20.0/sobjects/$objectType/$objectId";
-        $request = new HTTPRequest($url);
-        $request-> addHeaders("Authorization: OAuth $access_token");
-        $response->object = $request-> makeHTTPRequest();
-        $status = $request->getStatus();
-    
-        if ( $status != 200 )
-        {
-            $response->error = "Error: call to URL $url failed with status $status, response , curl_error " . 
-                    $request->getError() . ", curl_errno " . $request->getErrorNum();
-        }
-    }
-    catch(Exception $e){
-        $response->error = $e.getMessage();
-    }
-    return $response;
-}
-
-function getCustomerProfileIdFromSalesforce($contactId)
-{
-    $phpResponse = new stdClass();
-    $phpResponse->profileId = null;
-
-    $response = getOauthTokenWithPassword();
-    $json = $response->getPhpArray();
-    $_SESSION['access_token'] = $json['access_token'];
-    $_SESSION['instance_url'] = $json['instance_url'];
-    $response = getCustomerByContactId($contactId, $_SESSION['instance_url'],  $_SESSION['access_token']);
-    $response = json_decode($response->customer);
-
-    if(!empty($response->error))
-        $phpResponse->error = $response->error;
-    $phpResponse->profileId = $response->profileId__c;
-
-    return $phpResponse;
-}
-
-function getCustomerByContactId($contactId, $instance_url, $access_token) 
-{
-    try{
-        $response = new stdClass();
-        $response->customer = null;
-    
-        $url = "$instance_url/services/data/v20.0/sobjects/contact/$contactId";
-        $request = new HTTPRequest($url);
-        $request-> addHeaders("Authorization: OAuth $access_token");
-        $response->customer = $request-> makeHTTPRequest();
-        $status = $request->getStatus();
-    
-        if ( $status != 200 )
-        {
-            $response->error = "Error: call to URL $url failed with status $status, response , curl_error " . 
-                    $request->getError() . ", curl_errno " . $request->getErrorNum();
-        }
-    }
-    catch(Exception $e){
-        $response->error = $e.getMessage();
-    }
-
-    return $response;
-}
 
 function generateOrder($contactId, $pricebookEntryId)
 {
@@ -240,6 +79,52 @@ function generateOrder($contactId, $pricebookEntryId)
     }
     return $responseBody;
 }
-function salesforceTest(){
-    return "hello from salesforce test";
+
+
+
+
+
+
+function getCustomerProfileIdFromSalesforce($contactId)
+{
+    $phpResponse = new stdClass();
+    $phpResponse->profileId = null;
+
+    $response = getOauthTokenWithPassword();
+    $json = $response->getPhpArray();
+    $_SESSION['access_token'] = $json['access_token'];
+    $_SESSION['instance_url'] = $json['instance_url'];
+    $response = getCustomerByContactId($contactId, $_SESSION['instance_url'],  $_SESSION['access_token']);
+    $response = json_decode($response->customer);
+
+    if(!empty($response->error))
+        $phpResponse->error = $response->error;
+    $phpResponse->profileId = $response->profileId__c;
+
+    return $phpResponse;
+}
+
+function getCustomerByContactId($contactId, $instance_url, $access_token) 
+{
+    try{
+        $response = new stdClass();
+        $response->customer = null;
+    
+        $url = "$instance_url/services/data/v20.0/sobjects/contact/$contactId";
+        $request = new HTTPRequest($url);
+        $request-> addHeaders("Authorization: OAuth $access_token");
+        $response->customer = $request-> makeHTTPRequest();
+        $status = $request->getStatus();
+    
+        if ( $status != 200 )
+        {
+            $response->error = "Error: call to URL $url failed with status $status, response , curl_error " . 
+                    $request->getError() . ", curl_errno " . $request->getErrorNum();
+        }
+    }
+    catch(Exception $e){
+        $response->error = $e.getMessage();
+    }
+
+    return $response;
 }
