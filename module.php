cf4725772f5f7cc249c1a723154eee306bd1983c
ocdladefense <admin@ocdla.org>
2020-11-02 18:12:28 -0800

    Update module.php

--- module.php
+++ module.php
@@ -6,23 +6,44 @@ class SalesforceModule extends Module{
     public function __construct(){
         parent::__construct();
     }
+
+
+// NOTE: specific function here.  needs to be inside class...
+function generateOrder($contactId, $pricebookEntryId) {
+    $result = $this->soapRequest("CustomOrder","generateOrder");
+    
+    return $result;
 }
 
-function generateOrder($contactId, $pricebookEntryId)
-{
+/**
+ NOTE: gotta make this thing generic.
+ 
+ Go through it line by line.  Nothing should be hard-coded:
+   - Paths should not be hard-coded
+   - Class / Method names should not be hard coded
+ */
+function soapRequest($className, $methodName) {
     $responseBody = new stdClass();
     $responseBody->orderNumber = null;
 
-    require_once ('../vendor/developerforce/force.com-toolkit-for-php/soapclient/SforcePartnerClient.php');
-    require_once ('../vendor/developerforce/force.com-toolkit-for-php/soapclient/SforceHeaderOptions.php');
+    /**
+     * NOTE: you shouldn't need to include these.  These will get automatically loaded in via the Composer autoloader when
+       you require this repository in the appserver's root composer.json file.
+     */
+    // require_once ('../vendor/developerforce/force.com-toolkit-for-php/soapclient/SforcePartnerClient.php');
+    // require_once ('../vendor/developerforce/force.com-toolkit-for-php/soapclient/SforceHeaderOptions.php');
 
     $sfdc = new SforcePartnerClient();
     // create a connection using the partner wsdl
+    // NOTE: create a path to config in globals.php (i.e., get_config_path(); don't assume you can always get to config with ../config!
     $SoapClient = $sfdc->createConnection("../config/enterprise.wsdl");
     $loginResult = false;
 
     try {
         // log in with username, password and security token if required
+        // NOTE: Prepare config.php so you can connect to multiple Salesforce Orgs.
+        //  Having only one username/pass/token combo assumes there's only one Org to connect to. 
+        //  Might be several.
         $loginResult = $sfdc->login(SALESFORCE_USERNAME,SALESFORCE_PASSWORD,SECURITY_TOKEN);
     } catch (Exception $e) {
         $responseBody->error = "Failed to login to SforcePartnerClient". $e->faultstring;
@@ -31,8 +52,8 @@ function generateOrder($contactId, $pricebookEntryId)
     //Parse the URL and send it to the configFile
     $parsedURL = parse_url($sfdc->getLocation());
     define ("_SFDC_SERVER_", substr($parsedURL['host'],0,strpos($parsedURL['host'], '.')));
-    define ("_WS_NAME_", "CustomOrder");
-    define ("_WS_WSDL_", "../config/" . _WS_NAME_ . ".wsdl.xml");
+    define ("_WS_NAME_", "CustomOrder"); // NOTE: CustomOrder should not be hard-coded.  Pass it in via the route for now.
+    define ("_WS_WSDL_", "../config/" . _WS_NAME_ . ".wsdl.xml"); // NOTE: duplicate reference to wsdl file is above.  Fix.
     define ("_WS_ENDPOINT_", 'https://' . _SFDC_SERVER_ . '.salesforce.com/services/wsdl/class/' . _WS_NAME_);
     define ("_WS_NAMESPACE_", 'http://soap.sforce.com/schemas/class/' . _WS_NAME_);
 
@@ -43,8 +64,11 @@ function generateOrder($contactId, $pricebookEntryId)
     try 
     {
         // call the web service via post
-        $wsParams=array('customerId'=>$contactId,'pricebookEntryId' => $pricebookEntryId);
-        $response = $client->generateOrder($wsParams);
+        $params = array(
+                "customerId" => $contactId,
+                "pricebookEntryId" => $pricebookEntryId
+        );
+        $response = $client->generateOrder($wsParams); // NOTE: generateOrder should not be hard-coded but should be passed in via the route.
         $responseBody->orderNumber = $response->result;
     }
     catch (Exception $e) 
@@ -103,4 +127,8 @@ function getCustomerByContactId($contactId, $instance_url, $access_token)
     }
 
     return $response;
+}
+
+
+
 }
