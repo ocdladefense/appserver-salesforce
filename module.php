1c05c2efc76425c1bf0a4be6871852eb64a4e446
Jose Bernal <jbernal.web.dev@gmail.com>
2020-11-11 18:22:43 -0800

    Remove most constants which don't appear to be used anyways.

--- module.php
+++ module.php
@@ -1,16 +1,34 @@
 <?php
+/**
+ * Example module for executing Apex using the SOAP.
+ *
+ *
+ *  Example usage:
+ *    
+ *      $module = new SalesforceModule();
+ *
+ *			$data = $module->runReport("CurrentMembers");
+ *
+ *      file_save_contents($data, "/path/to/file");
+ **/
+
+
+use Force\SoapClient as SoapClient;
+use Force\SoapConnection as SoapConnection;
+
+
+// If this file is used outside of its framework,
+//  then create a class stub for Module.
+if(!class_exists("Module")) {
+
+	class Module {}
+}
+
+
 
 class SalesforceModule extends Module {
 
 		const DEFAULT_ORG_ALIAS = "myDefaultOrg";
-
-    private $sfdc;
-
-    private $client;
-
-    private $wsParams;
-
-    private $responseBody;
 
 
     public function __construct() {
@@ -18,8 +36,17 @@ class SalesforceModule extends Module {
     }
 
 
-		public function myTest() {
-			$this->invokeMethod("foobar","baz");
+		/**
+		 * Run an IABC report.
+		 *
+		 *  Execute a report on the IABC Salesforce instance.
+		 *  
+		 * @return - Returns a Blob or csv file, or array, that can be 
+		 *  saved and further processed.
+		 */
+    public function runReport($reportName) {
+    
+    
     }
     
     public function generateOrder($contactId, $pricebookEntryId, $org = null) {
@@ -43,29 +70,30 @@ class SalesforceModule extends Module {
 
   }
 
-		// , $apexClass, $apexMethod, $orgName
+
   public function invokeMethod($class, $method, $params = array(), $orgName = null) {
 
+		// Specify org configurations.
+		$orgName = $orgName ?: self::DEFAULT_ORG_ALIAS;
+		$org = load_org($orgName);		
+		
+		// Discover WSDL files.
+		$orgWsdl = path_to_wsdl("enterprise", $orgName);
+		$clientWsdl = path_to_wsdl($class, $orgName);
 
-				
 
-        $orgName = $orgName ?: self::DEFAULT_ORG_ALIAS;
-
-        $org = load_org($orgName);
-        
-        $sc = new SoapConnection($org["username"], $org["password"], $org["token"]);
-        
-        $result = $sc->execute($class, $method, $params);
-
-    }
+		$sc = new SoapConnection($orgWsdl);
+		$client = $sc->login($org["username"],$org["password"],$org["token"]);
+		$client->load($clientWsdl);
+		
+		
+		return $client->execute($class, $method, $params);
+	}
     
 
     
     
-    public function runReport($orgAlias, $reportName) {
-    
-    
-    }
+
 
 
 }
